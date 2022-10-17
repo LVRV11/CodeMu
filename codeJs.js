d7a0e15261270086e87719b0b71b9eb0c3bcac24
LVRV11 <lavrovdmitrii@gmail.com>
2022-10-17 16:30:30 +0200

    1

--- codeJs.js
+++ codeJs.js
@@ -1,1 +1,19 @@
-alert ("Hi");
+"use strict";
+// alert ("Hi");
+// let num = 123;
+// alert (num);
+// let a = 1+2+3;
+// alert (a);
+// let a = 10;
+// let b = 5;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert (result);
+
+// let a = 13;
+// let b = 5;
+// alert(a % b);
+
+let a = 3 * 2 ** 3;
+alert(a);
