8217e43698a2003c1a73700ea6ea4ad8f65411d0
LVRV11 <lavrovdmitrii@gmail.com>
2022-10-17 23:59:20 +0200

    d

--- codeJs.js
+++ codeJs.js
@@ -15,5 +15,74 @@
 // let b = 5;
 // alert(a % b);
 
-let a = 3 * 2 ** 3;
-alert(a);
+// let a = 3 * 2 ** 3;
+// alert(a);
+
+// let num = 123;
+// let str = '123';
+// console.log(num);
+// console.log(str);
+// alert(eee);
+
+// const PI = 3.14;
+// let r = 2.5;
+// r = 0.5;
+// let c = 2* PI * r;
+// alert (c);
+// r = 0.5;
+// alert (c);
+
+// let a = '5' + 2;
+// alert(a);
+// console.log(a);
+
+// let a = '' + 3 + 1;
+// alert(a);
+
+// let a = '10';
+// let b = '20';
+// let result = Number (a) + Number (b);
+// alert(result);
+
+// let a = '5px';
+// let b = '6px';
+// let result = parseInt (a) + parseInt (b) + 'px';
+// alert(result);
+
+// let a = '5.5px';
+// let b = '6.25px';
+// let result = parseFloat (a) + parseFloat (b) + 'px';
+// // let newResult = "'" + result + "'";
+// alert(result);
+// console.log(result);
+// console.log(newResult);
+
+// let num = 3421;
+// let numAnother = 1234321;
+// console.log(String(num).length + String(numAnother).length);
+
+// console.log(true + false);
+// console.log('1' + true);
+// console.log( String(true) + 1 );
+
+// let test = Boolean(3);
+// console.log(test);
+
+// let test = Boolean(1 / 0);
+// console.log(test);
+
+// let str = 'abcde';
+// console.log(str[0], str[2], str[4]);
+// let str = 'abcde';
+// let strNew = str[4] + str[3] + str[2] + str [1] + str[0];
+// console.log(strNew);
+
+// let str = 'abcde';
+// let num = 4;
+// console.log(str[num]);
+
+// let str = 'abcde';
+// console.log(str[str.length - 1]);
+// console.log(str[str.length - 2]);
+
+// let str = 'abcde';
