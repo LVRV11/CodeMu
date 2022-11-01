e3484b7482584431ba96de8bb8f1db92a4a6ffa8
LVRV11 <lavrovdmitrii@gmail.com>
2022-11-01 22:28:25 +0100

    2w

--- codeJs.js
+++ codeJs.js
@@ -180,7 +180,195 @@
 // let num2 = 2;
 // console.log('сумма: ' + (num1 + num2));
 
-let a = 1;
-let b = 2;
-let c = +prompt ('Vvedite chislo')
-console.log(a + b + c);
+// let a = 1;
+// let b = 2;
+// let c = +prompt ('Vvedite chislo')
+// console.log(a + b + c);
+
+// let num = '123';
+// let sum = +num[0] + +num[1] + +num[2];
+// console.log(sum);
+
+// let num = String(123);
+// console.log(num[0]);
+
+// let a = 0;
+// console.log(++a);
+
+// let num = String(123);
+// console.log(num.length);
+
+// let a = 60 * 60 * 24;
+// console.log(a);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.slice(-1));
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length-1]);
+
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + Number(b);
+
+// console.log(s);
+
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+
+// console.log(aaa + bbb + ccc);
+
+// let a = 60 * 60 * 24;
+// let sec = a * 30;
+// let secYear = a * 365;
+// let min = 60 * 24;
+// let minYear = min * 365;
+// let byte = 1e+6;
+// let byteG = 1e+9;
+// let byteInTenG = (1e+9)*10;
+// let byteTera = 1e+12;
+// let byteKilo = 1.25e+10;
+// console.log(a);
+// console.log(sec);
+// console.log(min);
+// console.log(minYear);
+// console.log(byte);
+// console.log(byteG);
+// console.log(byteInTenG);
+// console.log(byteKilo);
+
+// let r = 10;
+// let s = 2*r*3.14;
+// console.log(s);
+
+// let a = 10;
+// let s = a*a;
+// console.log(s);
+
+// let a = 10;
+// let b = 5;
+// let s = a*b;
+// console.log(s);
+
+// let tc = 36.6;
+// let tf = tc * 1.8 + 32;
+// console.log(tf);
+
+// let tf = 36.6;
+// let tc = (tf - 32)/1.8 ;
+// console.log(tc);
+// ______________________________________ARROW_____________________________________________
+
+// let arr = [1, 2, 3];
+// console.log(arr);
+// alert(arr);
+
+// let arr = ['Harlem', 'Amsterdam', 'Dalphson'];
+// console.log(arr);
+// alert(arr);
+
+// let arr = [1, 2, 3];
+// console.log(arr[0],arr[1],arr[2]);
+
+// let arr = [1, 2, 3];
+// console.log(arr[0] + arr[1] + arr[2]);
+
+// let arr = ['a', 'b', 'c', 'd'];
+// console.log(arr[0], '+', arr[1], '+',arr[2] , '+', arr[3] );
+
+// let arr = ['Harlem', 'Amsterdam', 'Dalphson'];
+// console.log(arr.length);
+// console.log(arr[arr.length-1]);
+
+// let arr = ['a', 'b', 'c'];
+// arr[0] = 1;
+// arr[1] = -2;
+// arr[2] = -3;
+// console.log(arr)
+
+// let arr = [1, 2, 3];
+// arr[0] += 3; 
+// arr[1] += 3;
+// arr[2] += 3;
+// console.log(arr);
+
+
+// let arr = [1, 2, 3];
+// arr[0]++; 
+// arr[1]++;
+// arr[2]++;
+// console.log(arr);
+
+// let arr = [];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// console.log(arr);
+
+// let arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(3);
+// console.log(arr);
+
+// let arr = [1, 2, 3];
+// arr.push(4);
+// arr.push(5);
+// console.log(arr);
+
+// let arr = ['a', 'b', 'c'];
+// let key = 2;
+// console.log(arr[key]);
+
+// let arr = [1, 2, 3, 4, 5];
+// let key1 = 1;
+// let key2 = 2;
+// console.log(arr[key1] + arr[key2]);
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// delete arr[2];
+// delete arr[4];
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// __________________________________________OBJECT______________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
