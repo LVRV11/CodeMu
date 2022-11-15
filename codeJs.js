63a26311f500b4b2e66653a7776dd489bd57a3c6
LVRV11 <lavrovdmitrii@gmail.com>
2022-11-15 22:33:04 +0100

    3e

--- codeJs.js
+++ codeJs.js
@@ -1785,3 +1785,130 @@
 // }
 
 // console.log(sum);
+
+// for (let i = 0; i <= 10; i++) {
+// 	console.log(i);
+// }
+
+// for (let i = 10; i > 0; i--) {
+// 	console.log(i);
+// }
+
+// for (let i = 10; i > 0; i--) {
+// 	console.log(i);
+// }
+
+// let i = 0;
+
+// while (i < 10) {
+//     i++;
+// 	console.log(i);
+	
+// }
+
+// let res = 0;
+
+// for (let i = 1; i <= 10; i++) {
+// 	res += i;
+// }
+
+// console.log(res);
+
+// let res = 1;
+
+// for (let i = 1; i <= 10; i++) {
+// 	res *= i;
+// }
+
+// console.log(res);
+
+
+// let arr = ['1', '2', '3', '4', '5'];
+// let sum = 0;
+
+// for (let elem of arr) {
+// 	sum += +elem;
+// }
+
+// console.log(sum); // должно вывести 15
+
+// let arr = ['1', '2', '3', '4', '5'];
+// let sum = Number('');
+
+// for (let elem of arr) {
+// 	sum += +elem;
+// }
+
+// console.log(sum); // должно вывести 15
+
+// let arr = ['1', '2', '3', '4', '5'];
+// let sum = 0;
+
+// for (let elem of arr) {
+// 	sum += +elem;
+// }
+
+// console.log(sum); // должно вывести 15
+
+// Код должен найти сумму элементов массива, однако, всегда выводит NaN:
+
+// let arr = ['1', '2', '3', '4', '5'];
+// let sum = 0;
+
+// for (let i = 0; i <= arr.length-1; i++) {
+// 	sum += +arr[i];
+// }
+
+// console.log(sum); 
+
+// let arr = ['1', '2', '3', '4', '5'];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+// 	sum += +arr[i];
+// }
+
+// console.log(sum); // почему-то выводит не 15
+
+// let arr = ['1', '2', '3', '4', '5'];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+// 	sum += +arr[i];
+// }
+
+// console.log(sum); // почему-то выводит не 15
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let elem of arr) {
+// 	arr = elem * elem;
+//     console.log(arr);
+// }
+
+// let arr = [];
+
+// for (let i = 1; i <= 5; i++) {
+// 	arr.push(i);
+// }
+
+// console.log(arr);
+
+// let obj = {a: 1, b: 2, c: 3};
+// let sum = 0;
+
+// for (let elem in obj) {
+// 	sum += obj[elem];
+// }
+
+// console.log(sum);
+
+// let obj = {a: 1, b: 2, c: 3};
+// let sum = 0;
+
+// for (let key in obj) {
+// 	sum += obj[key];
+// }
+
+// console.log(sum);
+
