16b689beb75920d849b2b639637338f854541c8f
LVRV11 <lavrovdmitrii@gmail.com>
2022-11-18 23:05:43 +0100

    1w

--- codeJs.js
+++ codeJs.js
@@ -2220,4 +2220,48 @@
 // let arr = [[1, 2], [3, 4], [5, 6]];
 // console.log((arr[0][0] + arr[0][1]) + (arr[1][0] + arr[1][1]) + ((arr[2][0] + arr[2][1])));
 
+// let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 10]];
+
+// for (let subArr of arr) {
+// 	for (let elem of subArr) {
+// 		console.log(elem);
+// 	}
+// }
+// let arr = [[1, 2, 3], [4, 5], [6]];
+// let sum = 0;
+// for (let subArr of arr){
+//     for (let elem of subArr){
+//         sum += elem;
+//     }
+// }
+// console.log(sum);
+
+// let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+// let sum = 0;
+// for (let subSubArr of arr){
+//         for (let subArr of subSubArr){
+//             for(let elem of subArr){
+//                 sum += elem;
+//             }
+//         }
+//     }
+//     console.log(sum);
+// let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++){
+//     for (let j =0; j < arr[i].length; i++){
+//         for(let k =0; k<arr[i][j]; k++)
+//         console.log(arr[i][j][k]);
+//     }
+// }
+
+// let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 10]];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	for (let j = 0; j < arr[i].length; j++) {
+// 		console.log(arr[i][j]);
+// 	}
+// }
+
+
 
