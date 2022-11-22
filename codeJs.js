e9db21afa979353e19cd799dff347257d764d4eb
LVRV11 <lavrovdmitrii@gmail.com>
2022-11-22 19:57:53 +0100

    1q

--- codeJs.js
+++ codeJs.js
@@ -2771,30 +2771,247 @@
 //     }
 // }
 
-let data = [
-	{
-		1: 'data11',
-		2: 'data12',
-		3: 'data13',
-	},
-	{
-		1: 'data21',
-		2: 'data22',
-		3: 'data33',
-	},
-	{
-		1: 'data31',
-		2: 'data32',
-		3: 'data33',
-	},
-];
+// let data = [
+// 	{
+// 		1: 'data11',
+// 		2: 'data12',
+// 		3: 'data13',
+// 	},
+// 	{
+// 		1: 'data21',
+// 		2: 'data22',
+// 		3: 'data33',
+// 	},
+// 	{
+// 		1: 'data31',
+// 		2: 'data32',
+// 		3: 'data33',
+// 	},
+// ];
 
-for (let elem of data){
-    // console.log(elem);
-    for (let key in elem){
-        console.log(key );
-    }
-    }
+// for (let elem of data){
+//     // console.log(elem);
+//     for (let key in elem){
+//         console.log(elem[key]);
+//     }
+//     }
   
-    
+// let employees = [
+// 	{
+// 		name: 'name1',
+// 		salary: 300,
+// 	},
+// 	{
+// 		name: 'name2',
+// 		salary: 400,
+// 	},
+// 	{
+// 		name: 'name3',
+// 		salary: 500,
+// 	},
+// ];
+
+// // Выведите на экран данные каждого работника в формате имя - зарплата.
+
+// for(let inf of employees){
+//     for(let key in inf){
+//         console.log(inf.name + '-' + inf.salary); 
+//     }
+// }
+
+// let employees = [
+//     	{
+//     		name: 'name1',
+//     		salary: 300,
+//     	},
+//     	{
+//     		name: 'name2',
+//     		salary: 400,
+//     	},
+//     	{
+//     		name: 'name3',
+//     		salary: 500,
+//     	},
+//     ];
+
+//  let sum = 0;
+//  for(let inf of employees){
+//         for(let key in inf){
+//             sum += inf.salary
+//             break;
+//         }
+//     }
+//     console.log(sum);
+
+// let employees = [
+// 	{
+// 		name: 'name1',
+// 		salary: 300,
+// 		age: 28,
+// 	},
+// 	{
+// 		name: 'name2',
+// 		salary: 400,
+// 		age: 29,
+// 	},
+// 	{
+// 		name: 'name3',
+// 		salary: 500,
+// 		age: 30,
+// 	},
+// 	{
+// 		name: 'name4',
+// 		salary: 600,
+// 		age: 31,
+// 	},
+// 	{
+// 		name: 'name5',
+// 		salary: 700,
+// 		age: 32,
+// 	},
+// ];
+
+// // Выведите на экран сумму зарплат тех работников, возраст которых равен или более 30 лет.
+
+// let sum = 0;
+// for(let inf of employees){
+//     for(let key in inf){
+//         if(inf.age>=30){
+//             sum += inf.salary;
+//             break;
+//         }
+//     }
+// }
+// console.log(sum);
+
+// let months = {
+// 	'ru': [
+// 		'январь',
+// 		'февраль',
+// 		'март',
+// 		'апрель',
+// 		'май',
+// 		'июнь',
+// 		'июль',
+// 		'август',
+// 		'сентябрь',
+// 		'октябрь',
+// 		'ноябрь',
+// 		'декабрь',
+// 	],
+// 	'en': [
+// 		'january',
+// 		'february',
+// 		'march',
+// 		'april',
+// 		'may',
+// 		'june',
+// 		'july',
+// 		'august',
+// 		'september',
+// 		'october',
+// 		'november',
+// 		'december',
+// 	],
+// };
+// let lang = 'ru'; // может быть или 'ru' или 'en'
+// let month = 5;   // число от 0 до 11
+
+// console.log(months[lang][5]);
+
+// let affairs = {
+// 	'2018': {
+// 		11: {
+// 			29: ['дело111', 'дело112', 'дело113'],
+// 			30: ['дело121', 'дело122', 'дело123'],
+// 		},
+// 		12: {
+// 			30: ['дело211', 'дело212', 'дело213'],
+// 			31: ['дело221', 'дело222', 'дело223'],
+// 		},
+// 	},
+// 	'2019': {
+// 		12: {
+// 			29: ['дело311', 'дело312', 'дело313'],
+// 			30: ['дело321', 'дело322', 'дело323'],
+// 			31: ['дело331', 'дело332', 'дело333'],
+// 		}
+// 	},
+// }
+// let year = '2019'; 
+// let month = 12; 
+// let affair = 30;  
+// console.log(affairs[year][month][affair]);
+
+// let obj = {
+// 	key1: {
+// 		key2: '12',
+// 		key3: '13',
+// 	},
+// 	key2: {
+// 		key4: '24',
+// 		key5: '25',
+// 	},
+// }
+
+// let key1 = 'key2';
+// let key2 = 'key4';
+// console.log(obj[key1][key2]);
+
+// let obj = {
+// 	key1: {
+// 		key2: '12',
+// 		key3: '13',
+// 	},
+// 	key2: {
+// 		key4: '24',
+// 		key5: '25',
+// 	},
+// }
+
+// let key1 = 'key2';
+// let key2 = 'key4' 
+// console.log(obj[key1][key2]);
+
+// let employees = [
+// 	{
+// 		name: 'name1',
+// 		salary: 300,
+// 		age: 28,
+// 	},
+// 	{
+// 		name: 'name2',
+// 		salary: 400,
+// 		age: 29,
+// 	},
+// 	{
+// 		name: 'name3',
+// 		salary: 500,
+// 		age: 30,
+// 	},
+// ];
+
+// employees.push({
+//      name: 'name4',
+// 		salary: 100,
+// 		age: 50,
+// });
+
+// console.log(employees);
+
+
+// let affairs = {
+// 	'2019-12-28': ['data11', 'data12', 'data13'],
+// 	'2019-12-29': ['data21', 'data22', 'data23'],
+// 	'2019-12-30': ['data31', 'data32', 'data33'],
+// }
+
+// let data = '2019-12-29';
+// affairs[data].push('data24');
+// let dataS = '2019-12-31';
+// affairs[dataS] = [];
+// affairs[dataS].push('data101', 'data102');
+// console.log(affairs);
+
+
 
