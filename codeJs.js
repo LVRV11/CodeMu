97352fb3bde1cbdbd26a7d994eb3eb41d728eb06
LVRV11 <lavrovdmitrii@gmail.com>
2022-11-24 19:56:04 +0100

    3q

--- codeJs.js
+++ codeJs.js
@@ -3278,12 +3278,34 @@
 // // console.log(arr.indexOf(3));
 // console.log(arr.includes(3));
 
-let str = 'london';
+// let str = 'london';
 
 // let result = str.slice(0, 1).toUpperCase() + str.slice(1);
 // let result = str.slice(0, -1) + str.slice(-1).toUpperCase();
 // let result = str.slice(0, 2).toUpperCase() + str.slice(2);
 // console.log(result); // выведет 'London'
 
-let str = 'London';
-console.log(str.toLowerCase());
+// let str = 'London';
+// console.log(str.toLowerCase());0
+
+// let str = 'word1 word2 word3';
+
+// // Разобьем строку в массив слов:
+// let words = str.split(' ');
+
+// for (let i = 0; i < words.length; i++) {
+// 	// Увеличим регистр каждого слова:
+// 	words[i] = words[i].slice(0, 1).toUpperCase() + words[i].slice(1);
+// }
+
+// // Сольем массив обратно в строку:
+// let result = words.join(' ');
+// console.log(result); // выведет 'Word1 Word2 Word3'
+
+let str = 'word1 word2 word3';
+let arr = str.split(' ');
+for( let i = 0; i < arr.length;i++){
+    arr[i]=arr[i].slice(0,1).toUpperCase() + arr[i].slice(1);
+}
+let res = arr.join(' ');
+console.log(res);
