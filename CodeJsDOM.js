05785a906b14f05841ba637f489ee6366c4b3e64
LVRV11 <lavrovdmitrii@gmail.com>
2023-02-15 15:46:34 +0100

    1q

--- CodeJsDOM.js
+++ CodeJsDOM.js
@@ -1573,3 +1573,56 @@ console.log(table)
         table.appendChild(tr);
      }
      console.log(table);
+
+
+     <body>
+    <table id="table"></table>
+<script>
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+let table = document.querySelector('table');
+
+for (let subArr of arr){
+    let tr = document.createElement('tr');
+    for (let elem of subArr){
+        let td = document.createElement('td');
+        td.textContent = elem;
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+</script>
+
+let employees = [
+	{name: 'employee1', age: 30, salary: 400},
+	{name: 'employee2', age: 31, salary: 500},
+	{name: 'employee3', age: 32, salary: 600},
+];
+
+let table = document.getElementById('table');
+
+for (let user of employees) {
+	let tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+    td1.textContent = user.name;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement('td');
+    td2.textContent = user.age;
+    td2.addEventListener('click', () => {
+        td2.textContent = +user.age + 1;
+    });
+    tr.appendChild(td2);
+    
+
+    let td3 = document.createElement('td');
+    td3.textContent = user.salary;
+    td3.addEventListener('click', () => {
+        td3.textContent = user.salary * 0.1 + user.salary;
+        console.log(user.salary * 0.1 + user.salary);
+    });
+    tr.appendChild(td3);
+
+    table.appendChild(tr);
+
+    }
